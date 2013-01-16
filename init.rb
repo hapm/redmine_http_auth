f89079fd01af90332bc6739a3c4d839b2573daa6
Steve Morrissey <uberamd@gmail.com>
2013-01-15 22:50:32 -0600

    groundwork for new auto accounts disabled on creation

--- init.rb
+++ init.rb
@@ -4,14 +4,10 @@ Rails.logger.info 'Starting Redmine Http Auth plugin for RedMine'
  
 Redmine::Plugin.register :redmine_http_auth do
   name 'HTTP Authentication plugin'
-  author 'Adam Lantos'
-  url 'http://github.com/AdamLantos/redmine_http_auth' if respond_to?(:url)
+  author 'Steve Morrissey forked from Adam Lantos'
+  url 'http://github.com/uberamd/redmine_http_auth' if respond_to?(:url)
   description 'A plugin for doing HTTP authentication'
-  version '0.3.0-dev-redmine-2.x'
-
-#  menu :account_menu, :login_httpauth, { :controller => 'httpauth-login' }, 
-#    :before => :login, :caption => :login_httpauth_title,
-#    :if => Proc.new { User.current.anonymous? && Setting.plugin_redmine_http_auth['enable'] == 'true' }
+  version '0.3.1-dev-redmine-2.x'
 
   settings :partial => 'settings/redmine_http_auth_settings',
     :default => {
@@ -20,6 +16,7 @@ Redmine::Plugin.register :redmine_http_auth do
       'lookup_mode' => 'login',
       'auto_registration' => 'false',
       'keep_sessions' => 'false'
+      'disable_new_accounts' => 'true',
     }
 end
 
